2b50b3cc1875290b4933ca2b3226d8fef704585c
Raghu K <raghuk.vit@gmail.com>
2023-03-22 07:31:32 +0530

    Update roboshop-kubernetes/

---
+++ schema/run.sh
@@ -0,0 +1,15 @@
+mkdir /code
+cd /code
+git clone https://github.com/raghudevopsb70/$COMPONENT .
+if [ $? -ne 0 ]; then
+  echo CLONE FAILED
+  exit 1
+fi
+
+cd /code/schema
+if [ $SCHEMA_TYPE == "mongodb" ]; then
+  curl -L -O  https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem
+  mongo --ssl --host ${DOCDB_ENDPOINT} --sslCAFile rds-combined-ca-bundle.pem --username ${DOCDB_USER}  --password ${DOCDB_PASS} < /code/schema/$COMPONENT.js
+elif [ $SCHEMA_TYPE == "mysql" ]; then
+  echo
+fi
